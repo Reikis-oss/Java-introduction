cda72c2b0f8326391997d9c8d2ae3648f4cb4573
Reikis-oss <ufnbkby@mail.ru>
2023-06-20 19:20:13 +0300

    Homework3

---
+++ Sem3/SolutSem3/src/App.java
@@ -0,0 +1,13 @@
+import Solution.HomeworkTask1;
+import Solution.HomeworkTask2;
+import Solution.HomeworkTask3;
+
+
+public class App {
+    public static void main(String[] args) throws Exception {
+        // HomeworkTask1.SortArray();
+        // HomeworkTask2.deleteNum(args);
+        HomeworkTask3.minimumAverageMaximumValue();
+
+    }
+}
